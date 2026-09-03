ff2704ceff57166ba2a00ffc33b9efdc351426a5
Xiongwei Lin <80614539+xiongweilin@users.noreply.github.com>
2026-09-04 07:36:33 +0800

    Expose cognitive controller API

---
+++ src/portable_runtime/controller/__init__.py
@@ -0,0 +1,15 @@
+from portable_runtime.controller.models import (
+    ControllerDecision,
+    ControllerDecisionKind,
+    ControllerState,
+    ControllerStatus,
+)
+from portable_runtime.controller.service import CognitiveController
+
+__all__ = [
+    "CognitiveController",
+    "ControllerDecision",
+    "ControllerDecisionKind",
+    "ControllerState",
+    "ControllerStatus",
+]
